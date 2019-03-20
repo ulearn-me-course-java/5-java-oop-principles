ad1b7f5cedc0e689a1533be8c3d43ac8acc3868f
Dementor911 <lhfujy7477@gmail.com>
2019-03-20 20:13:17 +0500

    Потапов Дмитрий. Задание 01.

---
+++ task01/src/com/example/task01/LevelOfImportance.java
@@ -0,0 +1,8 @@
+package com.example.task01;
+
+public enum LevelOfImportance {
+    DEBUG,
+    INFO,
+    WARNING,
+    ERROR
+}
---
+++ task01/src/com/example/task01/Logger.java
@@ -0,0 +1,85 @@
+package com.example.task01;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Logger {
+    public final String name;
+    public static Map<String, Logger> loggersName = new HashMap<>();
+    private LevelOfImportance level;
+
+
+    public Logger(String name) {
+        this.name = name;
+        loggersName.put(name, this);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public static Logger getLogger(String name) {
+        return loggersName.containsKey(name) ? loggersName.get(name)
+                : new Logger(name);
+    }
+
+    public void setLevel(LevelOfImportance level) {
+        this.level = level;
+    }
+
+    public LevelOfImportance getLevel() {
+        return this.level;
+    }
+
+    public void debug(String message) {
+        log(LevelOfImportance.DEBUG, message);
+    }
+
+    public void debug(String format, Object... args) {
+        log(LevelOfImportance.DEBUG, format, args);
+    }
+
+    public void info(String message) {
+        log(LevelOfImportance.INFO, message);
+    }
+
+    public void info(String format, Object... args) {
+        log(LevelOfImportance.INFO, format, args);
+    }
+
+    public void warning(String message) {
+        log(LevelOfImportance.WARNING, message);
+    }
+
+    public void warning(String format, Object... args) {
+        log(LevelOfImportance.WARNING, format, args);
+    }
+
+    public void error(String message) {
+        log(LevelOfImportance.ERROR, message);
+    }
+
+    public void error(String format, Object... args) {
+        log(LevelOfImportance.ERROR, format, args);
+    }
+
+    public void log(LevelOfImportance level, String message) {
+        if (this.level == null || (level != null && level.compareTo(this.level) <= 0)) {
+            DateFormat dateFormat = new SimpleDateFormat("dd.MM.YYYY hh:mm:ss");
+            Date date = new Date();
+            System.out.println(String.format("[%s] %s %s - %s", level, dateFormat.format(date), name, message));
+        }
+    }
+
+    public void log(LevelOfImportance level, String format, Object... args) {
+        if (level != null && this.level != null && level.compareTo(this.level) <= 0) {
+            String time = new SimpleDateFormat("dd.MM.YYYY_hh:mm:ss").format(Calendar.getInstance().getTime());
+            System.out.println(String.format("[%s] %s %s - %s", level, time, name, String.format(format, args)));
+        }
+    }
+
+}
