e4c4e03c7b252a2e4a07a027c7636d6577fe0b3f
Pavel <pavel.zaborin@inbox.ru>
2019-03-28 20:49:47 +0500

    Заборин Павел. Задание 03.

--- task03/src/com/example/task03/Seconds.java
+++ task03/src/com/example/task03/Seconds.java
@@ -23,6 +23,11 @@ public class Seconds implements TimeUnit {
 
     @Override
     public long toMinutes() {
-        return Math.round(amount / 60);
+        return Math.round((float)amount / 60);
+    }
+
+    @Override
+    public long toHours() {
+        return Math.round((float)amount / 3600);
     }
 }
