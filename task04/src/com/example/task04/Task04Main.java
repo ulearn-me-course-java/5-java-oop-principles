8d9a950b2a1d97e0fc8069afdfc15cf67c090564
Andrey Rakcheev <andre-orsk@yandex.ru>
2019-04-04 23:25:26 +0500

    Update Task04Main.java

--- task04/src/com/example/task04/Task04Main.java
+++ task04/src/com/example/task04/Task04Main.java
@@ -2,6 +2,11 @@ package com.example.task04;
 
 public class Task04Main {
     public static void main(String[] args) {
+        Logger logger = new Logger("MLogger");
 
+        logger.addHandler(new ConsoleHandler());
+        logger.addHandler(new ConsoleHandler());
+
+        logger.log(MessageLevel.DEBUG, "Simple message");
     }
 }
