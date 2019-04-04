97958074cb6a7725e13f97fb7706987d4701da9e
Andrey Rakcheev <andre-orsk@yandex.ru>
2019-04-04 23:23:51 +0500

    Update Task01Main.java

--- task01/src/com/example/task01/Task01Main.java
+++ task01/src/com/example/task01/Task01Main.java
@@ -2,6 +2,8 @@ package com.example.task01;
 
 public class Task01Main {
     public static void main(String[] args) {
-
+        Logger logger = new Logger("MLogger");
+        logger.log(MessageLevel.DEBUG, "Simple message");
+        Logger.getLogger("MLogger");
     }
 }
