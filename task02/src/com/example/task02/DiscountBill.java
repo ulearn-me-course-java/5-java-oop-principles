4db055357cdd7d3428fd0132d317129452e9f0c4
devil <devilaraxgada@gmail.com>
2019-11-04 15:31:28 +0500

    Задание 5.2

---
+++ task02/src/com/example/task02/DiscountBill.java
@@ -0,0 +1,26 @@
+package com.example.task02;
+
+public class DiscountBill extends Bill {
+    private double discount = 0;
+
+    public DiscountBill(double discount) {
+        if (discount < 0 || discount > 1) {
+            throw new IllegalArgumentException("Discount must be between 0 and 1");
+        }
+        this.discount = discount;
+    }
+
+    public double getDiscount() {
+        return discount * 100;
+    }
+
+    @Override
+    public long getPrice() {
+        long price = super.getPrice();
+        return (long) (price - price * (this.discount));
+    }
+
+    public double getAbsoluteDiscount() {
+        return super.getPrice() - this.getPrice();
+    }
+}
