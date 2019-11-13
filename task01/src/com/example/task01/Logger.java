a6f3a73dd3bd27125bb79b87be7dc32998222321
Валерия Самсонова <samsonova147@hotmail.com>
2019-11-13 16:17:55 +0500

    [Самсонова] Решение задачи 5.1

--- task01/src/com/example/task01/Logger.java
+++ task01/src/com/example/task01/Logger.java
@@ -7,7 +7,7 @@ import java.util.HashMap;
 
 import lombok.NonNull;
 
-enum LogLevels {
+enum LogLevel {
     DEBUG,
     INFO,
     WARNING,
@@ -15,7 +15,7 @@ enum LogLevels {
 }
 
 public class Logger {
-    private LogLevels Level;
+    private LogLevel level;
     private final String name;
     private static Map<String, Logger> loggers = new HashMap<String, Logger>();
 
@@ -24,62 +24,70 @@ public class Logger {
     }
 
     public static Logger getLogger(@NonNull String Name) {
-        Logger result = new Logger(Name);
-        loggers.put(result.name, result);
-        return result;
+        if (loggers.containsKey(Name)) {
+            return loggers.get(Name);
+        } else {
+            Logger result = new Logger(Name);
+            loggers.put(result.name, result);
+            return result;
+        }
     }
 
     public String getName() {
         return name;
     }
 
-    public LogLevels getLevel() {
-        return Level;
+    public LogLevel getLevel() {
+        return level;
     }
 
-    public void setLevel(@NonNull LogLevels level) {
-        Level = level;
+    public void setLevel(@NonNull LogLevel level) {
+        this.level = level;
     }
 
-    public void log(@NonNull LogLevels level, @NonNull String message) {
-        SimpleDateFormat sdfDate = new SimpleDateFormat("yyyy.MM.dd HH:mm:ss");
-        System.out.println("[" + level + "] " + sdfDate.format(new Date()) + " " + name + " - " + message);
+    public void log(@NonNull LogLevel level, @NonNull String message) {
+        if (this.level.compareTo(level) >= 0) {
+            SimpleDateFormat sdfDate = new SimpleDateFormat("yyyy.MM.dd HH:mm:ss");
+            System.out.println("[" + level + "] " + sdfDate.format(new Date()) + " " + name + " - " + message);
+        }
     }
 
-    public void log(@NonNull LogLevels level, @NonNull String message, @NonNull Object... obj) {
-        String msg = String.format(message, obj);
-        log(level, msg);
+    public void log(@NonNull LogLevel level, @NonNull String message, @NonNull Object... obj) {
+        if (this.level.compareTo(level) >= 0) {
+            String msg = String.format(message, obj);
+            log(level, msg);
+        }
     }
 
     public void debug(@NonNull String message) {
-        log(LogLevels.DEBUG, message);
+        log(LogLevel.DEBUG, message);
     }
 
     public void debug(@NonNull String message, @NonNull Object... obj) {
-        log(LogLevels.DEBUG, message, obj);
+        log(LogLevel.DEBUG, message, obj);
     }
 
     public void info(@NonNull String message) {
-        log(LogLevels.INFO, message);
+        log(LogLevel.INFO, message);
     }
 
     public void info(@NonNull String message, @NonNull Object... obj) {
-        log(LogLevels.INFO, message, obj);
+        log(LogLevel.INFO, message, obj);
     }
 
     public void warning(@NonNull String message) {
-        log(LogLevels.WARNING, message);
+        log(LogLevel.WARNING, message);
     }
 
     public void warning(@NonNull String message, @NonNull Object... obj) {
-        log(LogLevels.WARNING, message, obj);
+        log(LogLevel.WARNING, message, obj);
     }
 
     public void error(@NonNull String message) {
-        log(LogLevels.ERROR, message);
+        log(LogLevel.ERROR, message);
     }
 
     public void error(@NonNull String message, @NonNull Object... obj) {
-        log(LogLevels.ERROR, message, obj);
+        log(LogLevel.ERROR, message, obj);
     }
 }
