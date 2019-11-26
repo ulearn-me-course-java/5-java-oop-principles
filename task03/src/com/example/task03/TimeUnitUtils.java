8bfd28e0c06d8d655f0198583bfc415ee4f0d6e1
devil <devilaraxgada@gmail.com>
2019-11-26 20:45:29 +0500

    Add javadoc

--- task03/src/com/example/task03/TimeUnitUtils.java
+++ task03/src/com/example/task03/TimeUnitUtils.java
@@ -5,9 +5,43 @@ package com.example.task03;
  */
 public class TimeUnitUtils {
 
+    /**
+     * Переводит объект любого класса, реализующего интерфейс TimeUnit в Миллисекунды
+     *
+     * @param unit Объект класса, реализующего интерфейс TimeUnit
+     * @return Объект класса Milliseconds
+     */
+    public static Milliseconds toMillis(TimeUnit unit) {
+        return new Milliseconds(unit.toMillis());
+    }
 
-    public static Milliseconds toMillis(TimeUnit unit){return new Milliseconds(unit.toMillis());}
-    public static Seconds toSeconds(TimeUnit unit){return new Seconds(unit.toSeconds());}
-    public static Minutes toMinutes(TimeUnit unit){return new Minutes(unit.toMinutes());}
-    public static Hours toHours(TimeUnit unit){return new Hours(unit.toHours());}
+    /**
+     * Переводит объект любого класса, реализующего интерфейс TimeUnit в Секунды
+     *
+     * @param unit Объект класса, реализующего интерфейс TimeUnit
+     * @return Объект класса Seconds
+     */
+    public static Seconds toSeconds(TimeUnit unit) {
+        return new Seconds(unit.toSeconds());
+    }
+
+    /**
+     * Переводит объект любого класса, реализующего интерфейс TimeUnit в Минуты
+     *
+     * @param unit Объект класса, реализующего интерфейс TimeUnit
+     * @return Объект класса Minutes
+     */
+    public static Minutes toMinutes(TimeUnit unit) {
+        return new Minutes(unit.toMinutes());
+    }
+
+    /**
+     * Переводит объект любого класса, реализующего интерфейс TimeUnit в Часы
+     *
+     * @param unit Объект класса, реализующего интерфейс Hours
+     * @return Объект класса Milliseconds
+     */
+    public static Hours toHours(TimeUnit unit) {
+        return new Hours(unit.toHours());
+    }
 }
