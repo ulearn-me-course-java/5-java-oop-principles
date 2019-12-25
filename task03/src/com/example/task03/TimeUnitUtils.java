ecfc340a2ea66b9c2a6ed1306e346ab01ecc6489
AlexMaximov <dfkfljhy123@gmail.com>
2019-12-25 10:29:11 +0500

    Alexander Maximov task03

--- task03/src/com/example/task03/TimeUnitUtils.java
+++ task03/src/com/example/task03/TimeUnitUtils.java
@@ -6,22 +6,42 @@ package com.example.task03;
 public class TimeUnitUtils {
 
     /**
-     * Конвертирует интервал в секундах в интервал в миллисекундах
+     * Конвертирует TimeUnit интервал в интервал в миллисекундах
      *
-     * @param seconds интервал в секундах
+     * @param unit интервал TimeUnit
      * @return интервал в миллисекундах
      */
-    public static Milliseconds toMillis(Seconds seconds) {
-        return new Milliseconds(seconds.toMillis());
+    public static Milliseconds toMillis(TimeUnit unit) {
+        return new Milliseconds(unit.toMillis());
     }
 
     /**
-     * Конвертирует интервал в миллисекундах в интервал в секундах
+     * Конвертирует TimeUnit интервал в интервал в секундах
      *
-     * @param millis интервал в миллисекундах
+     * @param unit интервал TimeUnit
      * @return интервал в секундах
      */
-    public static Seconds toSeconds(Milliseconds millis) {
-        return new Seconds(millis.toSeconds());
+    public static Seconds toSeconds(TimeUnit unit) {
+        return new Seconds(unit.toSeconds());
+    }
+
+    /**
+     * Конвертирует TimeUnit интервал в интервал в минутах
+     *
+     * @param unit интервал TimeUnit
+     * @return интервал в минутах
+     */
+    public static Minutes toMinutes(TimeUnit unit) {
+        return new Minutes(unit.toMinutes());
+    }
+
+    /**
+     * Конвертирует TimeUnit интервал в интервал в часах
+     *
+     * @param unit интервал TimeUnit
+     * @return интервал в часах
+     */
+    public static Hours toHours(TimeUnit unit) {
+        return new Hours(unit.toHours());
     }
 }
