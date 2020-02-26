df24a74f6f123b075840ddc3fafaa3d3ca46782f
Grin <grinlikegreenbutgrin@gmail.com>
2020-02-26 21:54:27 +0500

    task #1

---
+++ task01/src/com/example/task01/Logger.java
@@ -0,0 +1,89 @@
+package com.example.task01;
+
+import java.text.MessageFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+
+public class Logger {
+    enum Level{
+        DEBUG,
+        INFO,
+        WARNING,
+        ERROR
+    }
+
+    private static HashMap<String, Logger> loggers;
+
+    static {
+        loggers = new HashMap<>();
+    }
+
+    private final String name;
+    private Level level = Level.DEBUG;
+
+    public Logger(String name){
+        this.name = name;
+        loggers.put(name, this);
+    }
+
+    public void setLevel(Level level){
+        this.level = level;
+    }
+
+    public Level getLevel(){
+        return level;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public static Logger getLogger(String name){
+        return loggers.containsKey("name") ? loggers.get(name) : new Logger(name);
+    }
+
+    public void debug(String template, Object... args){
+        log(Level.DEBUG, template, args);
+    }
+
+    public void info(String template, Object... args){
+        log(Level.INFO, template, args);
+    }
+
+    public void warning(String template, Object... args){
+        log(Level.WARNING, template, args);
+    }
+
+    public void error(String template, Object... args){
+        log(Level.ERROR, template, args);
+    }
+
+    public void debug(String message){
+        log(Level.DEBUG, message);
+    }
+
+    public void info(String message){
+        log(Level.INFO, message);
+    }
+
+    public void warning(String message){
+        log(Level.WARNING, message);
+    }
+
+    public void error(String message){
+        log(Level.ERROR, message);
+    }
+
+    public void log(Level level, String template, Object... args){
+        String message = MessageFormat.format(template, args);
+        if(level.ordinal() >= this.level.ordinal())
+            System.out.println(MessageFormat.format("[{0}] {1} {2} - {3}", level, new Date(), name, message));
+    }
+
+    public void log(Level level, String message){
+        SimpleDateFormat sdf = new SimpleDateFormat("YYYY.MM.dd HH:mm:ss");
+        if(level.ordinal() >= this.level.ordinal())
+            System.out.println(MessageFormat.format("[{0}] {1} {2} - {3}", level, sdf.format(new Date()), name, message));
+    }
+}
