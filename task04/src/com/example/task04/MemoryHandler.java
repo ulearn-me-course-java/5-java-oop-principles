a6388ac8a1c28a1258874c8d86cf9b8f45769e80
stasenash <staslisenash@gmail.com>
2020-03-13 00:14:38 +0500

    Бахарева Антонина З.04

--- task04/src/com/example/task04/MemoryHandler.java
+++ task04/src/com/example/task04/MemoryHandler.java
@@ -1,8 +1,26 @@
 package com.example.task04;
 
+import java.util.ArrayList;
+
 public class MemoryHandler implements  MessageHandler {
+    private MessageHandler handler;
+    private long maxSize;
+    private ArrayList<String> messageKeeper;
+
+    public MemoryHandler(MessageHandler handler, long maxSize)
+    {
+        this.handler = handler;
+        this.maxSize = maxSize;
+    }
+
     @Override
     public void log(String message) {
-
+        messageKeeper.add(message);
+        if (messageKeeper.size() >= maxSize)
+        {
+            for (String mes:messageKeeper)
+                handler.log(mes);
+            messageKeeper.clear();
+        };
     }
 }
--- task04/src/com/example/task04/RotationFileHandler.java
+++ task04/src/com/example/task04/RotationFileHandler.java
@@ -1,5 +1,7 @@
 package com.example.task04;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
 
@@ -14,8 +16,13 @@ public class RotationFileHandler implements MessageHandler {
     }
     @Override
     public void log(String message) {
-        LocalDateTime now =  LocalDateTime.now();
-        now.truncatedTo(rotation);
+        LocalDateTime now = LocalDateTime.now().truncatedTo(rotation);
+        try {
+            FileOutputStream fileOutputStream = new FileOutputStream("path"+ now.toString() + ".txt");
+            fileOutputStream.write(message.getBytes());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
