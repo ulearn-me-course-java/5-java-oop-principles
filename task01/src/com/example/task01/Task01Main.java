421548c0a0de7161af0ee01bbae3a66ecf150935
Id.Io.T <kdevyatkov-ilya@mail.ru>
2020-03-14 22:57:26 +0500

    task01

--- task01/src/com/example/task01/Task01Main.java
+++ task01/src/com/example/task01/Task01Main.java
@@ -1,7 +1,14 @@
 package com.example.task01;
 
+
+
 public class Task01Main {
     public static void main(String[] args) {
-
+        Logger logger = new Logger("ddd");
+        Logger logger1 = Logger.getLogger("ddd");
+        Logger logger2 = Logger.getLogger("ddd");
+        System.out.println(logger1 ==logger2);
     }
+
+
 }
