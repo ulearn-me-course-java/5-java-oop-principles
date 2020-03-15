1de3543857c6eae55fd80e7079b76fbd1df82f44
yeroxo <yeroxo@gmail.com>
2020-03-15 17:49:20 +0500

    Колыванов Артем При 201 task 5.1

---
+++ task01/src/com/example/task01/Logger.java
@@ -0,0 +1,87 @@
+package com.example.task01;
+
+import java.text.MessageFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+
+public class Logger {
+    enum Level{
+        DEBUG,
+        INFO,
+        WARNING,
+        ERROR
+    }
+
+    private static HashMap<String, Logger> loggers;
+    private static final SimpleDateFormat date_time = new SimpleDateFormat("YYYY.MM.dd HH:mm:ss");
+
+    static {
+        loggers = new HashMap<>();
+    }
+
+    private final String name;
+    private Level level = Level.DEBUG;
+
+    Logger(String name){
+        this.name = name;
+        loggers.put(name, this);
+    }
+
+    public void setLevel(Level level){
+        this.level = level;
+    }
+
+    Level getLevel(){
+        return level;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public static Logger getLogger(String name){
+        return loggers.containsKey("name") ? loggers.get(name) : new Logger(name);
+    }
+
+    public void debug(String template, Object... args){
+        log(Level.DEBUG, template, args);
+    }
+
+    public void info(String template, Object... args){
+        log(Level.INFO, template, args);
+    }
+
+    public void warning(String template, Object... args){
+        log(Level.WARNING, template, args);
+    }
+
+    public void error(String template, Object... args){
+        log(Level.ERROR, template, args);
+    }
+
+    public void debug(String message){
+        log(Level.DEBUG, message);
+    }
+
+    public void info(String message){
+        log(Level.INFO, message);
+    }
+
+    public void warning(String message){
+        log(Level.WARNING, message);
+    }
+
+    public void error(String message){
+        log(Level.ERROR, message);
+    }
+
+    private void log(Level level, String template, Object... args){
+        log(level, MessageFormat.format(template, args));
+    }
+
+    void log(Level level, String message){
+        if(level.ordinal() >= this.level.ordinal())
+            System.out.println(MessageFormat.format("[{0}] {1} {2} - {3}", level, date_time.format(new Date()), name, message));
+    }
+}
--- task01/src/com/example/task01/Task01Main.java
+++ task01/src/com/example/task01/Task01Main.java
@@ -2,6 +2,9 @@ package com.example.task01;
 
 public class Task01Main {
     public static void main(String[] args) {
-
+        Logger shit = new Logger("учеба");
+        System.out.println(shit.getLevel());
+        shit.log(Logger.Level.INFO,"много не спишь");
+        shit.log(Logger.Level.ERROR,"ошибка учебы");
     }
 }
