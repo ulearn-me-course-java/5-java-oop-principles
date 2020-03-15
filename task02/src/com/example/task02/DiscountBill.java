46fb21b10f6d321c53f13cddafc601680a5dfb2f
yeroxo <yeroxo@gmail.com>
2020-03-15 13:04:39 +0500

    Колыванов Артем При 201 task 5.2

---
+++ task02/src/com/example/task02/DiscountBill.java
@@ -0,0 +1,24 @@
+package com.example.task02;
+
+public class DiscountBill extends Bill {
+    private int discount;
+
+    DiscountBill(int discount){
+        if (discount >= 0 && discount <= 100)
+            this.discount = discount;
+    }
+
+    public int getDiscount() {
+        return discount;
+    }
+
+    public long absoluteDiscount(){
+        return super.getPrice() - this.getPrice();
+    }
+
+    @Override
+    public long getPrice() {
+        return (long)(super.getPrice()*(1 - (float)discount/100));
+    }
+}
+
