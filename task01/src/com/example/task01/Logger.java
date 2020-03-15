9e89db0fd090ea6df33688d47865986ec1db2fa1
Elizaveta Trushnikova <etsatsko@mail.ru>
2020-03-15 15:37:28 +0500

    Трушникова Елизавета 5.01

---
+++ task01/src/com/example/task01/Logger.java
@@ -0,0 +1,86 @@
+package com.example.task01;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+
+public class Logger
+{
+    enum Level{
+        DEBUG,
+        INFO,
+        WARNING,
+        ERROR
+    }
+
+    private String name;
+    private Level level;
+
+    public Logger(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    private static HashMap<String, Logger> loggers = new HashMap<>();
+
+    public static Logger getLogger(String name)
+    {
+        return loggers.containsKey("name") ? loggers.get(name) : new Logger(name);
+    }
+
+    public void setLevel(Level level) {
+        this.level = level;
+    }
+
+    public Level getLevel() {
+        return level;
+    }
+
+    public void debug(String message) {
+        log(Level.DEBUG, message);
+    }
+
+    public void debug(String message, Object... args) {
+        log(Level.DEBUG, message, args);
+    }
+
+    public void info(String message) {
+        log(Level.INFO, message);
+    }
+
+    public void info(String message, Object... args) {
+        log(Level.INFO, message, args);
+    }
+
+    public void warning(String message) {
+        log(Level.WARNING, message);
+    }
+
+    public void warning(String message, Object... args) {
+        log(Level.WARNING, message, args);
+    }
+
+    public void error(String message) {
+        log(Level.ERROR, message);
+    }
+
+    public void error(String message, Object... args) {
+        log(Level.ERROR, message, args);
+    }
+
+    public void log(Level level, String message, Object... args) {
+        log(level, String.format(message, args));
+    }
+
+    public void log(Level level, String message) {
+        if (level.ordinal() >= this.level.ordinal()){
+            Date dateNow = new Date();
+            String date = new SimpleDateFormat("yyyy.mm.dd").format(dateNow);
+            String time = new SimpleDateFormat("hh:mm:ss").format(dateNow);
+            System.out.println(String.format("[%d] %d %d  %d - %d", level, date, time, name, message));
+        }
+    }
+}
