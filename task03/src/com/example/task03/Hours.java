45f4a77e41441d9e1152233a92061547569158ac
Jin <jin1310@icloud.com>
2020-03-19 20:37:18 +0500

    task03

--- task03/src/com/example/task03/Hours.java
+++ task03/src/com/example/task03/Hours.java
@@ -1,30 +1,32 @@
 package com.example.task03;
 
-public class Minutes implements TimeUnit {
-
-private long amount;
-    public Minutes(long amount) {
+public class Hours implements TimeUnit {
+    private long amount;
+    public Hours(long amount) {
         this.amount=amount;
 
     }
 
     @Override
     public long toMillis() {
-        return amount*60000;
+        return amount*36000;
 
     }
 
     @Override
     public long toSeconds() {
-       return amount*60;
+        return amount*360;
     }
 
     @Override
     public long toMinutes() {
-        return amount;
+        return amount*60;
     }
     @Override
     public long toHours(){
-        return Math.round(amount/60);
+        return amount;
     }
 }
+
+
+
