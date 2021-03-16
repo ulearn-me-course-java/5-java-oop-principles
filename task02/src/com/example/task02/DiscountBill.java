737ea4e0a63b8c7be54f47b1e04c0ac253174ff8
Sofya <fiepaz03@gmail.com>
2021-03-16 17:41:52 +0500

    Task 5.1

---
+++ task02/src/com/example/task02/DiscountBill.java
@@ -0,0 +1,23 @@
+package com.example.task02;
+
+public class DiscountBill extends Bill {
+    private final int discount; //as a percentage
+
+    public int getDiscount() {
+        return discount;
+    }
+
+    public DiscountBill(int discount) {
+        super();
+        this.discount = discount;
+    }
+
+    public double getAbsolutDiscount() {
+        return super.getPrice() - getPrice();
+    }
+
+    @Override
+    public long getPrice() {
+        return Math.round(super.getPrice() * (100 - discount / 100f));
+    }
+}
