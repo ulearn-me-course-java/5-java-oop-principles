576926b95359c95f982eefb8f307b81ad5385443
Darina <37955869+dagenator@users.noreply.github.com>
2021-03-17 12:07:35 +0500

    Task03 Габидуллина

--- task03/src/com/example/task03/Seconds.java
+++ task03/src/com/example/task03/Seconds.java
@@ -23,6 +23,6 @@ public class Seconds implements TimeUnit {
 
     @Override
     public long toMinutes() {
-        return Math.round(amount / 60);
+        return Math.round((double)amount / 60);
     }
 }
