9e2eb328d73e364f6d57d550f73096d2da452819
1 <hstllbg@gmail.com>
2021-03-17 19:05:53 +0500

    Петров Игорь. Задание 5.2

---
+++ task02/src/com/example/task02/DiscountBill.java
@@ -0,0 +1,41 @@
+package com.example.task02;
+
+public class DiscountBill extends Bill {
+
+    private double discountPercentage;
+    private long discountValue;
+
+    public DiscountBill(double discountPercentage) {
+        this.discountPercentage = discountPercentage;
+    }
+
+    public double getDiscountPercent() {
+        return discountPercentage;
+    }
+
+    public long getDiscountValue() {
+        return discountValue;
+    }
+
+    public long getDiscountPrice() {
+        long price = getPrice();
+        discountValue = (long) (price * discountPercentage / 100);
+        return price - discountValue;
+    }
+
+    public void setDiscountPercentage(double discountPercentage) throws Exception {
+        if (discountPercentage < 0 || discountPercentage > 100)
+            throw new Exception("Неверное значение скидки");
+        this.discountPercentage = discountPercentage;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder(super.toString());
+        sb.append("\n");
+        sb.append("Сумма к оплате со скидкой (" + discountPercentage +"%): ").append(getDiscountPrice()).append("\n");
+        sb.append("Итого скидка составила: ").append(discountValue).append("\n");
+        return sb.toString();
+    }
+
+}
