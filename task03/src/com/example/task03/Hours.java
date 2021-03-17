10fc92b196e8b07082302df08aa8923af11c9a75
DarkLight <max.gubar@outlook.com>
2021-03-18 01:25:23 +0500

    Губарь Максим task3

--- task03/src/com/example/task03/Hours.java
+++ task03/src/com/example/task03/Hours.java
@@ -1,29 +1,28 @@
 package com.example.task03;
 
-public class Minutes implements TimeUnit {
-
+public class Hours implements TimeUnit{
     private final long amount;
 
-    public Minutes(long amount) {
+    public Hours(long amount) {
         this.amount = amount;
     }
 
     @Override
     public long toMillis() {
-        return amount * 60 * 1000;
+        return amount * 60 * 60 * 1000;
     }
 
     @Override
     public long toSeconds() {
-        return amount * 60;
+        return amount * 60 * 60;
     }
 
     @Override
     public long toMinutes() {
-        return amount;
+        return amount * 60;
     }
 
     public long getHours(){
-        return Math.round(amount / 60f);
+        return amount;
     }
 }
