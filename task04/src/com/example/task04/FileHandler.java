5b5c3b795a7c52026e9d495aa70054937527cb92
flumpyz <natalianata2001@mail.ru>
2021-03-18 16:53:27 +0500

    Лебедева Наталья. Задание 5.4

--- task04/src/com/example/task04/FileHandler.java
+++ task04/src/com/example/task04/FileHandler.java
@@ -8,67 +8,11 @@ import java.util.Date;
 import java.util.Dictionary;
 import java.util.Hashtable;
 
-public class FileHandler implements MessageHandler {
-
-    private static final Dictionary<String, FileHandler> loggers = new Hashtable<>();
-    private final String name;
-    private Level level;
+public class FileHandler extends Logger
+        implements MessageHandler {
 
     public FileHandler(String name) {
-        this.name = name;
-        loggers.put(name, this);
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public static FileHandler getLogger(String name) {
-        if (loggers.get(name) != null) {
-            return loggers.get(name);
-        } else {
-            return new FileHandler(name);
-        }
-    }
-
-    public void setLevel(Level desiredLevel) {
-        level = desiredLevel;
-    }
-
-    public Level getLevel() {
-        return level;
-    }
-
-    public void debug(String message) {
-        log(Level.DEBUG, message);
-    }
-
-    public void debug(String template, Object... args) {
-        log(Level.DEBUG, template, args);
-    }
-
-    public void info(String message) {
-        log(Level.INFO, message);
-    }
-
-    public void info(String template, Object... args) {
-        log(Level.INFO, template, args);
-    }
-
-    public void warning(String message) {
-        log(Level.WARNING, message);
-    }
-
-    public void warning(String template, Object... args) {
-        log(Level.WARNING, template, args);
-    }
-
-    public void error(String message) {
-        log(Level.ERROR, message);
-    }
-
-    public void error(String template, Object... args) {
-        log(Level.ERROR, template, args);
+        super(name);
     }
 
     @Override
@@ -78,7 +22,7 @@ public class FileHandler implements MessageHandler {
             FileWriter writer = new FileWriter(file, true);
             Date now = new Date();
             SimpleDateFormat nowFormat = new SimpleDateFormat("yyyy.MM.dd hh:mm:ss");
-            writer.write("[" + logLevel + "] " + nowFormat.format(now) + " " + name + " - " + message + "\n");
+            writer.write("[" + logLevel + "] " + nowFormat.format(now) + " " + getName() + " - " + message + "\n");
 
             writer.close();
         } catch (IOException ex) {
--- task04/src/com/example/task04/MemoryHandler.java
+++ task04/src/com/example/task04/MemoryHandler.java
@@ -3,82 +3,27 @@ package com.example.task04;
 import java.text.SimpleDateFormat;
 import java.util.*;
 
-public class MemoryHandler implements MessageHandler {
+public class MemoryHandler extends Logger
+        implements MessageHandler {
 
-    private static final Dictionary<String, MemoryHandler> loggers = new Hashtable<>();
-    private final String name;
-    private Level level;
-    private Queue<String> logs;
+    private final Queue<String> logs;
     private final int bufferSize;
 
     public MemoryHandler(String name, int bufferSize) {
-        this.name = name;
-        loggers.put(name, this);
+        super(name);
         logs = new ArrayDeque<>();
         this.bufferSize = bufferSize;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public static MemoryHandler getLogger(String name) {
-        if (loggers.get(name) != null) {
-            return loggers.get(name);
-        } else {
-            return new MemoryHandler(name, 4);
-        }
-    }
-
-    public void setLevel(Level desiredLevel) {
-        level = desiredLevel;
-    }
-
-    public Level getLevel() {
-        return level;
     }
 
     public int getBufferSize() {
         return bufferSize;
     }
 
-    public void debug(String message) {
-        log(Level.DEBUG, message);
-    }
-
-    public void debug(String template, Object... args) {
-        log(Level.DEBUG, template, args);
-    }
-
-    public void info(String message) {
-        log(Level.INFO, message);
-    }
-
-    public void info(String template, Object... args) {
-        log(Level.INFO, template, args);
-    }
-
-    public void warning(String message) {
-        log(Level.WARNING, message);
-    }
-
-    public void warning(String template, Object... args) {
-        log(Level.WARNING, template, args);
-    }
-
-    public void error(String message) {
-        log(Level.ERROR, message);
-    }
-
-    public void error(String template, Object... args) {
-        log(Level.ERROR, template, args);
-    }
-
     @Override
     public void log(Level logLevel, String message) {
         Date now = new Date();
         SimpleDateFormat nowFormat = new SimpleDateFormat("yyyy.MM.dd hh:mm:ss");
-        String text = ("[" + logLevel + "] " + nowFormat.format(now) + " " + name + " - " + message);
+        String text = ("[" + logLevel + "] " + nowFormat.format(now) + " " + getName() + " - " + message);
         if (logs.size() < bufferSize) {
             logs.offer(text);
         } else {
