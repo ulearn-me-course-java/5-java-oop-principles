5d9989e914bcd3c1e52d9d9560c227aeba45ce42
пк <nikitos.wie@mail.ru>
2021-03-24 17:49:32 +0300

    Яхин, Task - 1

--- task01/src/com/example/task01/Task01Main.java
+++ task01/src/com/example/task01/Task01Main.java
@@ -1,5 +1,12 @@
 package com.example.task01;
 
+    enum Level {
+        Debug,
+        Info,
+        Warning,
+        Error
+    }
+
 public class Task01Main {
     public static void main(String[] args) {
 
