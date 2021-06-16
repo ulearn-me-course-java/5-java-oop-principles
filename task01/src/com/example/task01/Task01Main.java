0a8cf161cbcc6b6b7577af4b0452a3a0605c2d4b
rewqty <boss.dima0000@gmail.com>
2021-06-16 19:14:03 +0500

    Челноков Дмитрий task01

--- task01/src/com/example/task01/Task01Main.java
+++ task01/src/com/example/task01/Task01Main.java
@@ -2,6 +2,11 @@ package com.example.task01;
 
 public class Task01Main {
     public static void main(String[] args) {
-
+        Logger logger = new Logger("TestError");
+        logger.setLevel(Logger.Level.ERROR);
+        logger.debug("MessageDebugTest");
+        logger.info("MessageInfoTest");
+        logger.warning("MessageWarningTest");
+        logger.error("MessageErrorTest");
     }
 }
