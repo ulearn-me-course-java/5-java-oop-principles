fba97c628b94bfd6b7d4bc660fa8bb969868e4b1
IlyaIIS <ilya1221221@mail.ru>
2022-03-20 22:04:33 +0500

    Исмаков Илья - task 1

--- task01/src/com/example/task01/Task01Main.java
+++ task01/src/com/example/task01/Task01Main.java
@@ -2,6 +2,8 @@ package com.example.task01;
 
 public class Task01Main {
     public static void main(String[] args) {
-
+        Logger logger = Logger.getLogger("testLogger");
+        logger.setLevel(Level.INFO);
+        Logger.getLogger("testLogger").warning("(%s)(%s)", logger.getLevel(), logger.getName());
     }
 }
