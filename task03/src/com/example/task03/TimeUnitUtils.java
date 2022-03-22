9112326f6a95d254f0e16869e85edf582d322fe8
Maxim <machsim26@gmail.com>
2022-03-22 21:01:03 +0500

    Complete

--- task03/src/com/example/task03/TimeUnitUtils.java
+++ task03/src/com/example/task03/TimeUnitUtils.java
@@ -24,4 +24,8 @@ public class TimeUnitUtils {
     public static Seconds toSeconds(Milliseconds millis) {
         return new Seconds(millis.toSeconds());
     }
+
+    public static Minutes toMinutes(Seconds seconds) { return new Minutes(seconds.toMinutes()); }
+    public static Hours toHours(Minutes minutes) { return new Hours(minutes.toHours()); }
+    // и т.д.
 }
