a01082015edf3bd73a429de72ebad8257602d757
Sekou Samassa <71543096+sksamassa@users.noreply.github.com>
2022-03-23 01:48:10 +0500

    Update Seconds.java

--- task03/src/com/example/task03/Seconds.java
+++ task03/src/com/example/task03/Seconds.java
@@ -23,6 +23,11 @@ public class Seconds implements TimeUnit {
 
     @Override
     public long toMinutes() {
-        return Math.round(amount / 60);
+        return Math.round((float)amount / (float)60);
+    }
+
+    @Override
+    public long toHours(){
+        return Math.round((float)amount / (float)3600);
     }
 }
