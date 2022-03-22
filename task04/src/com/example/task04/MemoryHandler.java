a7b65bffab7777a8c4b15c9a9303d6adca3d8443
evabronskayaa <eva27122002b@gmail.com>
2022-03-23 00:21:44 +0500

    added task04

---
+++ task04/src/com/example/task04/MemoryHandler.java
@@ -0,0 +1,28 @@
+package com.example.task04;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class MemoryHandler implements MessageHandler {
+    private final int maxSize;
+    private final List<String> messages = new ArrayList<>();
+    private final List<MessageHandler> handlers;
+
+    public MemoryHandler(int maxSize, MessageHandler... handlers) {
+        this.maxSize = maxSize;
+        this.handlers = Arrays.asList(handlers);
+    }
+
+    @Override
+    public void log(String msg) {
+        messages.add(msg);
+        if (maxSize >= messages.size()) {
+            for (MessageHandler handler : handlers) {
+                for (String message : messages)
+                    handler.log(message);
+                messages.clear();
+            }
+        }
+    }
+}
