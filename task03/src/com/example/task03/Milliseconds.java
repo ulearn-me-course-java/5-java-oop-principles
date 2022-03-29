67ff2b99a06b880df1e955febd30c2120007930c
WEBMaximWEB <maxim.geer@yandex.ru>
2022-03-29 17:40:06 +0500

    Геер Максим. Task3

--- task03/src/com/example/task03/Milliseconds.java
+++ task03/src/com/example/task03/Milliseconds.java
@@ -18,11 +18,16 @@ public class Milliseconds implements TimeUnit {
 
     @Override
     public long toSeconds() {
-        return amount / 1000;
+        return Math.round(amount / 1000d);
     }
 
     @Override
     public long toMinutes() {
-        return amount / 1000 * 60;
+        return Math.round(amount / 60_000d);
+    }
+
+    @Override
+    public long getHours(){
+        return Math.round(amount / 3_600_000d);
     }
 }
