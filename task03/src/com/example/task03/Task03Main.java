13e03c70ae6108537d60cde1dc33ae6c2d217a1b
Denis <Barkas074@yandex.ru>
2022-03-30 00:45:48 +0500

    task03. Completed.

--- task03/src/com/example/task03/Task03Main.java
+++ task03/src/com/example/task03/Task03Main.java
@@ -3,7 +3,11 @@ package com.example.task03;
 public class Task03Main {
     public static void main(String[] args) {
         TimeUnit unit1 = new Seconds(1000);
+        TimeUnit unit2 = new Minutes(59);
+        TimeUnit unit3 = new Hours(140);
         printTimeUnit(unit1);
+        printTimeUnit(unit2);
+        printTimeUnit(unit3);
     }
 
     private static void printTimeUnit(TimeUnit unit) {
--- task03/src/com/example/task03/Tests.java
+++ task03/src/com/example/task03/Tests.java
@@ -12,82 +12,95 @@ public class Tests {
      * @param expectedSeconds ожидаемое количество секунд
      * @param expectedMinutes ожидаемое количество минут
      */
-    private void testTimeUnit(TimeUnit timeUnit, long expectedMillis, long expectedSeconds, long expectedMinutes) {
+    private void testTimeUnit(TimeUnit timeUnit, long expectedMillis, long expectedSeconds, long expectedMinutes, long expectedHours) {
         long actualMillis = timeUnit.toMillis();
         long actualSeconds = timeUnit.toSeconds();
         long actualMinutes = timeUnit.toMinutes();
+        long actualHours = timeUnit.getHours();
         Assert.assertEquals("Количество миллисекунд не соответствует ожидаемому", expectedMillis, actualMillis);
         Assert.assertEquals("Количество секунд не соответствует ожидаемому", expectedSeconds, actualSeconds);
         Assert.assertEquals("Количество минут не соответствует ожидаемому", expectedMinutes, actualMinutes);
+        Assert.assertEquals("Количество часов не соответствует ожидаемому", expectedHours, actualHours);
     }
 
     @Test
     public void testMilliseconds() {
-        testTimeUnit(new Milliseconds(1000), 1000, 1, 0);
+        testTimeUnit(new Milliseconds(1000), 1000, 1, 0, 0);
     }
 
     @Test
     public void testMilliseconds2() {
-        testTimeUnit(new Milliseconds(29999), 29999, 30, 0);
+        testTimeUnit(new Milliseconds(29999), 29999, 30, 0, 0);
     }
 
     @Test
     public void testMilliseconds3() {
-        testTimeUnit(new Milliseconds(30000), 30000, 30, 1);
+        testTimeUnit(new Milliseconds(30000), 30000, 30, 1, 0);
     }
 
     @Test
     public void testMilliseconds4() {
-        testTimeUnit(new Milliseconds(1000000), 1000000, 1000, 17);
+        testTimeUnit(new Milliseconds(1000000), 1000000, 1000, 17, 0);
     }
 
     @Test
     public void testSeconds() {
-        testTimeUnit(new Seconds(60), 60000, 60, 1);
+        testTimeUnit(new Seconds(60), 60000, 60, 1, 0);
     }
 
     @Test
     public void testSeconds2() {
-        testTimeUnit(new Seconds(29), 29000, 29, 0);
+        testTimeUnit(new Seconds(29), 29000, 29, 0, 0);
     }
 
     @Test
     public void testSeconds3() {
-        testTimeUnit(new Seconds(30), 30000, 30, 1);
+        testTimeUnit(new Seconds(30), 30000, 30, 1, 0);
     }
 
     @Test
     public void testMinutes() {
-        testTimeUnit(new Minutes(10), 600000, 600, 10);
+        testTimeUnit(new Minutes(10), 600000, 600, 10, 0);
     }
 
     @Test
     public void testMinutes2() {
-        testTimeUnit(new Minutes(70), 4200000, 4200, 70);
+        testTimeUnit(new Minutes(70), 4200000, 4200, 70, 1);
     }
 
     @Test
     public void testUtils() {
         Seconds seconds = TimeUnitUtils.toSeconds(new Milliseconds(1500));
-        testTimeUnit(seconds, 2000, 2, 0);
+        testTimeUnit(seconds, 2000, 2, 0, 0);
     }
 
     @Test
     public void testUtils2() {
         Seconds seconds = TimeUnitUtils.toSeconds(new Milliseconds(1499));
-        testTimeUnit(seconds, 1000, 1, 0);
+        testTimeUnit(seconds, 1000, 1, 0, 0);
     }
 
     @Test
     public void testUtils3() {
         Milliseconds millis = TimeUnitUtils.toMillis(new Seconds(29));
-        testTimeUnit(millis, 29000, 29, 0);
+        testTimeUnit(millis, 29000, 29, 0, 0);
     }
 
     @Test
     public void testUtils4() {
         Milliseconds millis = TimeUnitUtils.toMillis(new Seconds(30));
-        testTimeUnit(millis, 30000, 30, 1);
+        testTimeUnit(millis, 30000, 30, 1, 0);
     }
 
+    @Test
+    public void testUtils5() {
+        Minutes minutes = TimeUnitUtils.toMinutes(new Seconds(59));
+        testTimeUnit(minutes, 60000, 60, 1, 0);
+    }
+
+    @Test
+    public void testUtils6() {
+        Hours hours = TimeUnitUtils.toHours(new Minutes(59));
+        testTimeUnit(hours, 3600000, 3600, 60, 1);
+    }
 }
