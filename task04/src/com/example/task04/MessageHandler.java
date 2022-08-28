ce2254535076a2c3093464ed4f0c1ea89f094880
modina-o@mail.ru <wbXUk*StPRN5Agx>
2022-08-28 20:04:07 +0500

    task04 Модина

---
+++ task04/src/com/example/task04/MessageHandler.java
@@ -0,0 +1,12 @@
+package com.example.task04;
+
+/**
+ * Интерфейс для конечной обработки сообщений логгера
+ */
+public interface MessageHandler {
+    /**
+     *
+     */
+    void log(String message);
+}
+
