3ed908ad96bae9228668a1ae08de7ee350afa8da
Alina <wbrflf3301@outlook.com>
2022-10-23 19:02:27 +0500

    Исправления

--- task01/src/com/example/task01/Logger.java
+++ task01/src/com/example/task01/Logger.java
@@ -7,97 +7,65 @@ import java.util.Map;
 
 public class Logger {
     private final String _name;
-    private LogSeverityLvl _severityLvl;
+    private LogSeverityLvl _currentLvl;
     private final Date _date;
     private static final Map<String, Logger> map = new HashMap<>();
 
     public Logger(String name) {
-        _name = name; _severityLvl = LogSeverityLvl.DEBUG; _date = new Date();
+        _name = name; _currentLvl = LogSeverityLvl.DEBUG; _date = new Date();
     }
 
     public String getName() { return _name; }
 
-    public void setLevel(LogSeverityLvl lvl) { _severityLvl = lvl; }
+    public void setLevel(LogSeverityLvl lvl) { _currentLvl = lvl; }
 
-    public LogSeverityLvl getLevel() { return _severityLvl; }
+    public LogSeverityLvl getLevel() { return _currentLvl; }
 
-    public void log(LogSeverityLvl lvl, String message) throws Exception {
-        switch(lvl)
-        {
-            case DEBUG:
-                debug(message);     break;
-            case INFO:
-                info(message);      break;
-            case WARNING:
-                warning(message);   break;
-            case ERROR:
-                error(message);     break;
-            default:
-                throw new Exception("Такого уровня важности не существует!");
-        }
+    public void log(LogSeverityLvl lvl, String message) {
+        printMessage(lvl, message);
     }
 
-    public void log(LogSeverityLvl lvl, String format, Object ... params) throws Exception {
-        switch(lvl)
-        {
-            case DEBUG:
-                debug(format, params);     break;
-            case INFO:
-                info(format, params);      break;
-            case WARNING:
-                warning(format, params);   break;
-            case ERROR:
-                error(format, params);     break;
-            default:
-                throw new Exception("Такого уровня важности не существует!");
-        }
+    public void log(LogSeverityLvl lvl, String format, Object ... params) {
+        printMessage(lvl, format, params);
     }
 
     public void debug(String message) {
-        System.out.printf("[%s] %s %s %s - %s%n",
-                LogSeverityLvl.DEBUG,
-                new SimpleDateFormat("yyyy.MM.dd").format(_date),
-                new SimpleDateFormat("hh:mm:ss").format(_date),
-                getName(),
-                message
-        );
+        printMessage(LogSeverityLvl.DEBUG, message);
     }
 
     public void debug(String format, Object... params) {
-        System.out.printf((format) + "%n", params);
+        printMessage(LogSeverityLvl.DEBUG, format, params);
     }
 
     public void info(String message) {
-        System.out.printf("[%s] %s %s %s - %s%n",
-                LogSeverityLvl.INFO,
-                new SimpleDateFormat("yyyy.MM.dd").format(_date),
-                new SimpleDateFormat("hh:mm:ss").format(_date),
-                getName(),
-                message
-        );
+        printMessage(LogSeverityLvl.INFO, message);
     }
 
     public void info(String format, Object ... params) {
-        System.out.printf((format) + "%n", params);
+        printMessage(LogSeverityLvl.INFO, format, params);
     }
 
     public void warning(String message) {
-        System.out.printf("[%s] %s %s %s - %s%n",
-                LogSeverityLvl.WARNING,
-                new SimpleDateFormat("yyyy.MM.dd").format(_date),
-                new SimpleDateFormat("hh:mm:ss").format(_date),
-                getName(),
-                message
-        );
+        printMessage(LogSeverityLvl.WARNING, message);
     }
 
     public void warning(String format, Object ... params) {
-        System.out.printf((format) + "%n", params);
+        printMessage(LogSeverityLvl.WARNING, format, params);
     }
 
     public void error(String message) {
+        printMessage(LogSeverityLvl.ERROR, message);
+    }
+
+    public void error(String format, Object ... params) {
+        printMessage(LogSeverityLvl.ERROR, format, params);
+    }
+
+    private void printMessage(LogSeverityLvl lvl, String message) {
+        if(_currentLvl.ordinal() > lvl.ordinal()) return;
+
         System.out.printf("[%s] %s %s %s - %s%n",
-                LogSeverityLvl.ERROR,
+                lvl,
                 new SimpleDateFormat("yyyy.MM.dd").format(_date),
                 new SimpleDateFormat("hh:mm:ss").format(_date),
                 getName(),
@@ -105,7 +73,9 @@ public class Logger {
         );
     }
 
-    public void error(String format, Object ... params) {
+    private void printMessage(LogSeverityLvl lvl, String format, Object... params) {
+        if(_currentLvl.ordinal() > lvl.ordinal()) return;
+
         System.out.printf((format) + "%n", params);
     }
 
