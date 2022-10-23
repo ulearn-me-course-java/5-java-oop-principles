4b96e05b57de775a448d2244f24a534ea3913b77
YesNile <holmovdanil2@gmail.com>
2022-10-24 00:16:32 +0500

    Lab5

--- task03/src/com/example/task03/Minutes.java
+++ task03/src/com/example/task03/Minutes.java
@@ -1,27 +1,35 @@
 package com.example.task03;
 
 public class Minutes implements TimeUnit {
+    private final long amount;
 
     public Minutes(long amount) {
         // TODO: реализовать
-        throw new UnsupportedOperationException();
+        this.amount = amount;
+        //throw new UnsupportedOperationException();
     }
 
     @Override
     public long toMillis() {
         // TODO: реализовать
-        throw new UnsupportedOperationException();
+        return amount * 60 * 1000;
+        //throw new UnsupportedOperationException();
     }
 
     @Override
     public long toSeconds() {
         // TODO: реализовать
-        throw new UnsupportedOperationException();
+        return this.amount * 60;
+        //throw new UnsupportedOperationException();
     }
 
     @Override
     public long toMinutes() {
         // TODO: реализовать
-        throw new UnsupportedOperationException();
+        return this.amount;
+        //throw new UnsupportedOperationException();
+    }public long toHours() {
+        return Math.round(this.amount / 60f);
     }
+
 }
