fe23d0c913f88d69405ff065d30bb8334e956e29
Danila <Deplodoctor@gmail.com>
2022-10-23 19:50:16 +0500

    Lab5 end

--- task03/src/com/example/task03/TimeUnitUtils.java
+++ task03/src/com/example/task03/TimeUnitUtils.java
@@ -5,23 +5,53 @@ package com.example.task03;
  */
 public class TimeUnitUtils {
 
-    /**
-     * Конвертирует интервал в секундах в интервал в миллисекундах
-     *
-     * @param seconds интервал в секундах
-     * @return интервал в миллисекундах
-     */
-    public static Milliseconds toMillis(Seconds seconds) {
-        return new Milliseconds(seconds.toMillis());
-    }
 
-    /**
-     * Конвертирует интервал в миллисекундах в интервал в секундах
-     *
-     * @param millis интервал в миллисекундах
-     * @return интервал в секундах
-     */
-    public static Seconds toSeconds(Milliseconds millis) {
-        return new Seconds(millis.toSeconds());
+    public static Milliseconds toMillis(Milliseconds value) {
+        return new Milliseconds(value.toMillis());
+    }
+    public static Milliseconds toMillis(Seconds value) {
+        return new Milliseconds(value.toMillis());
+    }
+    public static Milliseconds toMillis(Minutes value) {
+        return new Milliseconds(value.toMillis());
+    }
+    public static Milliseconds toMillis(Hours value) {
+        return new Milliseconds(value.toMillis());
+    }
+    public static Seconds toSeconds(Milliseconds value) {
+        return new Seconds(value.toSeconds());
+    }
+    public static Seconds toSeconds(Seconds value) {
+        return new Seconds(value.toSeconds());
+    }
+    public static Seconds toSeconds(Minutes value) {
+        return new Seconds(value.toSeconds());
+    }
+    public static Seconds toSeconds(Hours value) {
+        return new Seconds(value.toSeconds());
+    }
+    public static Minutes toMinutes(Milliseconds value) {
+        return new Minutes(value.toMinutes());
+    }
+    public static Minutes toMinutes(Seconds value) {
+        return new Minutes(value.toMinutes());
+    }
+    public static Minutes toMinutes(Minutes value) {
+        return new Minutes(value.toMinutes());
+    }
+    public static Minutes toMinutes(Hours value) {
+        return new Minutes(value.toMinutes());
+    }
+    public static Hours toHours(Milliseconds value) {
+        return new Hours(value.toHours());
+    }
+    public static Hours toHours(Seconds value) {
+        return new Hours(value.toHours());
+    }
+    public static Hours toHours(Minutes value) {
+        return new Hours(value.toHours());
+    }
+    public static Hours toHours(Hours value) {
+        return new Hours(value.toHours());
     }
 }
