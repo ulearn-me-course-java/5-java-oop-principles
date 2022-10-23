b6cfbb969056b855eb2538da6286bf99e2882578
Your Soul <timurnigmatulin6@gmail.com>
2022-10-23 23:41:39 +0500

    task2

--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -10,12 +10,13 @@ public class Task02Main {
     private static final Item ITEM6 = new Item("Товар 6", 60);
 
     public static void main(String[] args) {
-        Bill bill = new Bill();
-        bill.add(ITEM1, 10);
-        bill.add(ITEM3, 3);
-        bill.add(ITEM6, 1);
-        System.out.println(bill);
-        bill.add(ITEM3, 3);
-        System.out.println(bill);
+        DiscountBill discountBill = new DiscountBill(10);
+        discountBill.add(ITEM1, 10);
+        discountBill.add(ITEM3, 3);
+        discountBill.add(ITEM6, 1);
+        System.out.println(discountBill.getPrice());
+        System.out.println(discountBill.getDiscount());
+        System.out.println(discountBill.getAbsoluteDiscount());
+
     }
 }
