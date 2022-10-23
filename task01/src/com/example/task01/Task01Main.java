eebb9f828be3fcd2622546584da4490929df4177
Настя <nastamezenceva9155@gmail.com>
2022-10-23 21:32:24 +0500

    all

--- task01/src/com/example/task01/Task01Main.java
+++ task01/src/com/example/task01/Task01Main.java
@@ -3,5 +3,12 @@ package com.example.task01;
 public class Task01Main {
     public static void main(String[] args) {
 
+        Logger logger = new Logger("test");
+
+        Logger logger1 = Logger.getLogger("test");
+        Logger logger2 = Logger.getLogger("test");
+        boolean f =  logger1 == logger2;
+
+        System.out.println(f);
     }
 }
---
+++ task04/src/com/example/task04/Logger.java
@@ -0,0 +1,102 @@
+package com.example.task04;
+
+import java.text.MessageFormat;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Logger{
+    private final String name;
+    private LoggerLevel level;
+    private static final Map<String, Logger> loggerMap = new HashMap<>();
+
+    public Logger(String name){
+        this.name = name;
+        this.level = LoggerLevel.INFO;
+        loggerMap.put(name, this);
+    }
+
+    public Logger(String name, LoggerLevel level) {
+        this.name = name;
+        this.level = level;
+        loggerMap.put(name, this);
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setLevel(LoggerLevel level){
+        this.level = level;
+    }
+
+    public LoggerLevel getLevel(){
+        return level;
+    }
+
+    public static Logger getLogger(String name){
+        Logger logger = loggerMap.get(name);
+        if (logger == null) {
+            logger = new Logger(name);
+        }
+        return logger;
+    }
+
+    public void debug(String message) {
+        log(LoggerLevel.DEBUG, message);
+    }
+
+    public void debug(String template, Object... args) {
+        log(LoggerLevel.DEBUG, template, args);
+    }
+
+    public void info(String message) {
+        log(LoggerLevel.INFO, message);
+    }
+
+    public void info(String template, Object... args) {
+        log(LoggerLevel.INFO, template, args);
+    }
+
+    public void warning(String message) {
+        log(LoggerLevel.WARNING, message);
+    }
+
+    public void warning(String template, Object... args) {
+        log(LoggerLevel.WARNING, template, args);
+    }
+
+    public void error(String message) {
+        log(LoggerLevel.ERROR, message);
+    }
+
+    public void error(String template, Object... args) {
+        log(LoggerLevel.ERROR, template, args);
+    }
+
+    public void log(LoggerLevel level, String message) {
+        printMessage(level, message);
+    }
+
+    public void log(LoggerLevel level, String template, Object... args) {
+        printMessage(level, template, args);
+    }
+
+    private void printMessage(LoggerLevel level, String message) {
+        if (level.ordinal() >= this.level.ordinal()) {
+           System.out.println(MessageFormat.format("[{0}] {1} {2} - {3}",
+                    level,
+                    LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy.MM.dd hh:mm:ss")),
+                    this.name,
+                    message));
+        }
+
+    }
+    private void printMessage(LoggerLevel level, String template, Object... args) {
+        if (level.ordinal() >= this.level.ordinal()) {
+            System.out.println(MessageFormat.format(template, args));
+        }
+    }
+
+}
