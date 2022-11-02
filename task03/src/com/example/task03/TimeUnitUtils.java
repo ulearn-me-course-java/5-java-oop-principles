c1781d7c7e56ef14f24d68f07a740277ec2d6139
Veronika Dyminskaya <veroni4ka_d03@mail.ru>
2022-11-02 23:40:36 +0500

    Task-3

--- task03/src/com/example/task03/TimeUnitUtils.java
+++ task03/src/com/example/task03/TimeUnitUtils.java
@@ -5,23 +5,44 @@ package com.example.task03;
  */
 public class TimeUnitUtils {
 
-    /**
-     * Конвертирует интервал в секундах в интервал в миллисекундах
-     *
-     * @param seconds интервал в секундах
-     * @return интервал в миллисекундах
-     */
     public static Milliseconds toMillis(Seconds seconds) {
         return new Milliseconds(seconds.toMillis());
     }
+    public static Milliseconds toMillis(Minutes minutes) {
+        return new Milliseconds(minutes.toMillis());
+    }
+    public static Milliseconds toMillis(Hours hours) {
+        return new Milliseconds(hours.toMillis());
+    }
 
-    /**
-     * Конвертирует интервал в миллисекундах в интервал в секундах
-     *
-     * @param millis интервал в миллисекундах
-     * @return интервал в секундах
-     */
+
     public static Seconds toSeconds(Milliseconds millis) {
         return new Seconds(millis.toSeconds());
+    }
+    public static Seconds toSeconds(Minutes minutes) {
+        return new Seconds(minutes.toSeconds());
+    }
+    public static Seconds toSeconds(Hours hours) {
+        return new Seconds(hours.toSeconds());
+    }
+
+    public static Minutes toMinutes(Milliseconds millis) {
+        return new Minutes(millis.toSeconds());
+    }
+    public static Minutes toMinutes(Seconds seconds) {
+        return new Minutes(seconds.toSeconds());
+    }
+    public static Minutes toMinutes(Hours hours) {
+        return new Minutes(hours.toSeconds());
+    }
+
+    public static Hours toHours(Milliseconds millis) {
+        return new Hours(millis.toSeconds());
+    }
+    public static Hours toHours(Seconds seconds) {
+        return new Hours(seconds.toSeconds());
+    }
+    public static Hours toHours(Minutes minutes) {
+        return new Hours(minutes.toSeconds());
     }
 }
