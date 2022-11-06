5d60d63c8ddd8812b892794303a1d14dced007a6
Vilena <vilena.saitgalina@mail.ru>
2022-11-07 03:30:46 +0500

    task 3

--- task03/src/com/example/task03/TimeUnitUtils.java
+++ task03/src/com/example/task03/TimeUnitUtils.java
@@ -14,6 +14,8 @@ public class TimeUnitUtils {
     public static Milliseconds toMillis(Seconds seconds) {
         return new Milliseconds(seconds.toMillis());
     }
+    public static Minutes toMinutes(Seconds seconds) { return new Minutes(seconds.toMinutes()); }
+    public static Hours toHours(Seconds seconds) { return new Hours(seconds.toHours()); }
 
     /**
      * Конвертирует интервал в миллисекундах в интервал в секундах
@@ -24,4 +26,16 @@ public class TimeUnitUtils {
     public static Seconds toSeconds(Milliseconds millis) {
         return new Seconds(millis.toSeconds());
     }
+    public static Minutes toMinutes(Milliseconds millis) { return new Minutes(millis.toMinutes()); }
+    public static Hours toHours(Milliseconds millis) { return new Hours(millis.toHours()); }
+
+
+    public static Milliseconds toMillis(Minutes minutes) { return new Milliseconds(minutes.toMillis()); }
+    public static Seconds toSeconds(Minutes minutes) { return new Seconds(minutes.toSeconds()); }
+    public static Hours toHours(Minutes minutes) { return new Hours(minutes.toHours()); }
+
+
+    public static Milliseconds toMillis(Hours hours) { return new Milliseconds(hours.toMillis()); }
+    public static Seconds toSeconds(Hours hours) { return  new Seconds(hours.toSeconds()); }
+    public static Minutes toMinutes(Hours hours) { return new Minutes(hours.toMinutes()); }
 }
