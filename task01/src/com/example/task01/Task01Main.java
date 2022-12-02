50bf28c4199ff09b0b21777c822c6b080d85bae8
pryseolg <oleg.kuts.02@mail.ru>
2022-12-02 14:23:04 +0500

    Task 1 complete. Kuts Oleg

--- task01/src/com/example/task01/Task01Main.java
+++ task01/src/com/example/task01/Task01Main.java
@@ -2,6 +2,19 @@ package com.example.task01;
 
 public class Task01Main {
     public static void main(String[] args) {
+        String word = "wdwd";
+        String word1 = "wfofl";
+        Logger logger1 = new Logger("error of life");
+        Logger logger2 = new Logger("error of life");
+        Logger logger3 = new Logger("Cringe");
+        Logger logger4 = new Logger("No, please");
+        logger4.warning("This is fake %s, %s", word, word1);
+        logger4.setLevel(Level.ERROR);
+        logger4.warning("This is fake %s, %s", word, word1);
+
+        Logger logger10 = Logger.getLogger("test");
+        Logger logger20 = Logger.getLogger("test");
+        System.out.println(logger10 == logger20);
 
     }
 }
