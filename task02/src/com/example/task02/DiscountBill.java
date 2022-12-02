13662c92c55b0e02b8b23ca9c55da5bf189e9fe0
pryseolg <oleg.kuts.02@mail.ru>
2022-12-02 14:27:14 +0500

    Task 2 complete. Kuts Oleg

---
+++ task02/src/com/example/task02/DiscountBill.java
@@ -0,0 +1,24 @@
+package com.example.task02;
+
+public class DiscountBill extends Bill{
+    private int discount;
+
+    public DiscountBill(int discount) {
+        if (discount >= 0 && discount <= 100) {
+            this.discount = discount;
+        }
+    }
+
+    public int getDiscount(){
+        return discount;
+    }
+
+    public long getAbsolute() {
+        return super.getPrice() - this.getPrice();
+    }
+
+    @Override
+    public long getPrice(){
+        return (long) (super.getPrice() * (1 - discount / 100f));
+    }
+}
