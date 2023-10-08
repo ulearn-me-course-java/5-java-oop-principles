e4892bcae67db14a92d8f39010e121d20c26a665
FazylovV <vladislav.fazylov04@gmail.com>
2023-10-08 22:18:09 +0500

    task1 complete 99%

--- task01/src/com/example/task01/Task01Main.java
+++ task01/src/com/example/task01/Task01Main.java
@@ -1,7 +1,12 @@
 package com.example.task01;
 
+import java.sql.Time;
+
 public class Task01Main {
     public static void main(String[] args) {
-
+        Logger logger = new Logger("test");
+        logger.setLevel(Logger.Level.DEBUG);
+        logger.debug("debug smth");
+        logger.error("Database is not available now");
     }
 }
