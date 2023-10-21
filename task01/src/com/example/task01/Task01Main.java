aff0d4b98729509c4c378d1e6ba41a2bfd753afd
alwk <Xcom.D@yandex.ru>
2023-10-21 22:26:57 +0500

    Задание_1.Калентьев Дмитрий

--- task01/src/com/example/task01/Task01Main.java
+++ task01/src/com/example/task01/Task01Main.java
@@ -2,6 +2,9 @@ package com.example.task01;
 
 public class Task01Main {
     public static void main(String[] args) {
-
+        Logger logger = new Logger("test");
+        Logger logger1 = Logger.getLogger("test");
+        Logger logger2 = Logger.getLogger("test");
+        System.out.println(logger1 == logger2);
     }
 }
