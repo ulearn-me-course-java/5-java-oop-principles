a12a08f3c48e7ad364718d0fc8197d902a636a39
FazylovV <vladislav.fazylov04@gmail.com>
2023-10-21 22:58:26 +0500

    MemoryHandler done

--- task04/src/com/example/task04/MemoryHandler.java
+++ task04/src/com/example/task04/MemoryHandler.java
@@ -1,9 +1,35 @@
 package com.example.task04;
 
+import com.sun.org.apache.xalan.internal.xsltc.runtime.MessageHandler;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class MemoryHandler implements Handler {
+    private ArrayList<Handler> handlers;
+    private ArrayList<String> messages;
+    private final int bufferSize;
+
+    public MemoryHandler(int bufferSize, Handler... handlers) {
+        this.bufferSize = bufferSize;
+        this.messages = new ArrayList<>();
+        this.handlers.addAll(Arrays.asList(handlers));
+    }
 
     @Override
     public void log(String message) {
-        return;
+        messages.add(message);
+        if(messages.size() >= bufferSize) {
+            logMessages();
+        }
+    }
+
+    private void logMessages(){
+        for (Handler handler : handlers) {
+            for (String message : messages) {
+                handler.log(message);
+            }
+        }
+        messages.clear();
     }
 }
