ad8d7ac1694f6d0c982f116bcc650ff4846b4942
Dying <dyingvoid@gmail.com>
2023-10-22 11:49:00 +0500

    log methods

--- task01/src/com/example/task01/Task01Main.java
+++ task01/src/com/example/task01/Task01Main.java
@@ -1,7 +1,9 @@
 package com.example.task01;
 
 public class Task01Main {
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
+        Logger logger = new Logger("Vlad");
+        logger.log(Level.DEBUG, "%s %d", "Me", 10);
     }
 }
