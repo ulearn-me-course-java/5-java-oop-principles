ba9aba66add1c62ab6b0c3c828610a7a9642b103
Dying <dyingvoid@gmail.com>
2023-10-22 17:12:50 +0500

    fourth

--- task04/src/com/example/task04/Task04Main.java
+++ task04/src/com/example/task04/Task04Main.java
@@ -2,6 +2,14 @@ package com.example.task04;
 
 public class Task04Main {
     public static void main(String[] args) {
+        Logger logger = new Logger();
 
+        logger.registerLogger(new ConsoleLogger());
+        logger.registerLogger(new FileLogger("logFile"));
+        logger.registerLogger(new RotationLogger());
+        logger.registerLogger(new MemoryLogger(new ConsoleLogger()));
+
+        logger.log(Level.DEBUG, "meme");
+        logger.log(Level.DEBUG, "meme");
     }
 }
