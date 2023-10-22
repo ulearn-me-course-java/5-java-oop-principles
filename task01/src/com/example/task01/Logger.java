259b03e5893fb50dbe40e98b934c96591b14460a
Dying <dyingvoid@gmail.com>
2023-10-22 12:23:41 +0500

    first

--- task01/src/com/example/task01/Logger.java
+++ task01/src/com/example/task01/Logger.java
@@ -8,6 +8,11 @@ import java.util.Date;
 public class Logger {
     private static ArrayList<Logger> loggers = new ArrayList<>();
     private String name;
+    private Level level = Level.DEBUG;
+
+    public Logger(){
+        Logger.loggers.add(this);
+    }
 
     public Logger(String name){
         this.name = name;
@@ -18,26 +23,41 @@ public class Logger {
         return name;
     }
 
-    public static Logger getLogger(String _name){
-        Logger fLogger = null;
+    public void setName(String _name){
+        name = _name;
+    }
 
+    public Level getLevel(){
+        return level;
+    }
+
+    public void setLevel(Level _level){
+        level = _level;
+    }
+
+    public static Logger getLogger(String _name){
         for(Logger logger : loggers){
-            if(logger.getName() == _name){
-                fLogger = logger;
-                break;
+            if(logger.getName().equals(_name)){
+                return logger;
             }
         }
 
-        return fLogger;
+        return new Logger(_name);
     }
 
     public void log(Level level, String message){
+        if(level.ordinal() < this.level.ordinal())
+            return;
+
         System.out.printf(
                 "[%s] %s %s - %s%n", level, getDateTime(), getName(), message
         );
     }
 
     public void log(Level level, String message, Object... args){
+        if(level.ordinal() < this.level.ordinal())
+            return;
+
         String someStr = String.format("[%s] %s %s - ", level, getDateTime(), getName());
         System.out.printf(someStr + (message) + "%n", args);
     }
@@ -48,5 +68,37 @@ public class Logger {
         DateFormat df = new SimpleDateFormat(pattern);
 
         return df.format(date);
+    }
+
+    public void error(String message){
+        log(Level.ERROR, message);
+    }
+
+    public void error(String message, Object... args){
+        log(Level.ERROR, message, args);
+    }
+
+    public void warning(String message){
+        log(Level.WARNING, message);
+    }
+
+    public void warning(String message, Object... args){
+        log(Level.WARNING, message, args);
+    }
+
+    public void info(String message){
+        log(Level.INFO, message);
+    }
+
+    public void info(String message, Object... args){
+        log(Level.INFO, message, args);
+    }
+
+    public void debug(String message){
+        log(Level.DEBUG, message);
+    }
+
+    public void debug(String message, Object... args){
+        log(Level.DEBUG, message, args);
     }
 }
--- task01/src/com/example/task01/Task01Main.java
+++ task01/src/com/example/task01/Task01Main.java
@@ -3,7 +3,6 @@ package com.example.task01;
 public class Task01Main {
 
     public static void main(String[] args) {
-        Logger logger = new Logger("Vlad");
-        logger.log(Level.DEBUG, "%s %d", "Me", 10);
+
     }
 }
