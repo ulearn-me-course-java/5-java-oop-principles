d7ae5706c0fbf7caf7937d9777b8f229da6760ca
evan_kirk <evankirk@vk.com>
2023-11-05 18:09:31 +0500

    Киров 04

--- task04/src/com/example/task04/Task04Main.java
+++ task04/src/com/example/task04/Task04Main.java
@@ -1,7 +1,22 @@
 package com.example.task04;
 
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+
 public class Task04Main {
     public static void main(String[] args) {
 
+        ArrayList<MessageHandler> handlers = new ArrayList<>();
+
+        handlers.add(new ConsoleHandler());
+        handlers.add(new FileHandler());
+        handlers.add(new RotationFileHandler(ChronoUnit.SECONDS));
+        handlers.add(new MemoryHandler(5, handlers));
+
+        Logger logger = new Logger("test", handlers);
+        Logger logger2 = new Logger("secondTest", Level.WARNING, handlers);
+
+        logger.info("ORA ORA ORA ORA");
+        logger2.warning("MUDA MUDA MUDA MUDA");
     }
 }
