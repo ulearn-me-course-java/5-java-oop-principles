8551c22ee5b8886ea15b48c215cb7999e06a8c89
evan_kirk <evankirk@vk.com>
2023-11-05 17:04:15 +0500

    Киров 03

--- task03/src/com/example/task03/Hours.java
+++ task03/src/com/example/task03/Hours.java
@@ -1,33 +1,30 @@
 package com.example.task03;
 
-/**
- * Интервал в секундах
- */
-public class Seconds implements TimeUnit {
+public class Hours implements TimeUnit {
 
     private final long amount;
 
-    public Seconds(long amount) {
+    public Hours(long amount) {
         this.amount = amount;
     }
 
     @Override
     public long toMillis() {
-        return amount * 1000;
+        return amount * 3600 * 1000;
     }
 
     @Override
     public long toSeconds() {
-        return amount;
+        return amount * 3600;
     }
 
     @Override
     public long toMinutes() {
-        return Math.round(amount / 60f);
+        return amount * 60;
     }
 
     @Override
     public long toHours() {
-        return Math.round(amount / 3600f);
+        return amount;
     }
 }
