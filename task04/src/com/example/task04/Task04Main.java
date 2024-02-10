df3553f0bd89188bcd3ffc6a687f98adbfb6ceba
LoiLok <artemklpkv@gmail.com>
2024-02-10 23:33:31 +0500

    task04

--- task04/src/com/example/task04/Task04Main.java
+++ task04/src/com/example/task04/Task04Main.java
@@ -2,6 +2,9 @@ package com.example.task04;
 
 public class Task04Main {
     public static void main(String[] args) {
-
+        Logger logger = new Logger("test1", Logger.Level.WARNING, new ConsoleHandler());
+        logger.setLevel(Logger.Level.DEBUG);
+        logger.debug("test2");
+        logger.error("test3");
     }
 }
